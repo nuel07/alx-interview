148a77f9cf7dc0def7ef9a5bb7dd18f85532fd82
nuel07 <yehanganep@gmail.com>
2022-04-14 06:04:23 -0700

    task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''determines if all the boxes can be opened.'''
+
+
+def canUnlockAll(boxes):
+    '''determines if all boxes have keys and
+    returns True if all boxes can be opened.
+    '''
+    keys = [0]
+    for idx, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key in boxes[idx] and key not in keys and key != idx:
+                keys.append(key)
+    if len(keys) == len(boxes):
+        return True
+    return False
