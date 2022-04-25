1cddd45f2bdf70c134fa310202fed34d29636708
nuel07 <yehanganep@gmail.com>
2022-04-25 05:14:50 -0700

    task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+''' calculate minimum number of operations on a file '''
+
+
+def minOperations(n: int) -> int:
+    ''' returns minimum number of operations
+    Args:
+        n(int): number of H characters
+    Return: int
+    '''
+    num_ops = 0
+    ops = 2 ## copy All and Paste
+    while n > 1:
+        while n % ops == 0:
+            num_ops += ops
+            n /= ops
+        ops += 1
+    return num_ops
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,3 @@
+# 0x02-minimum_operations
+Another interview preparation project, focusing on number of
+operations on a given file.
