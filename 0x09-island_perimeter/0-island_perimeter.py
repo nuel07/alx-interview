61a2f5270aa17c1204e70ab4cdb64e76fcf008e4
nuel07 <yehanganep@gmail.com>
2022-06-07 09:20:04 -0700

    pep correction

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,18 +2,18 @@
 """ Module for determining island perimeter """
 
 
-def island_perimeter(grid):    
+def island_perimeter(grid):
     """returns island perimeter"""
     width = len(grid)
-    height = len(grid[0])    
+    height = len(grid[0])
     perimeter = 0
 
-    for r in range(width):        
-        for c in range(height):            
-            if grid[r][c] == 1:                
-                perimeter += 4                
-                if r > 0 and grid[r-1][c] == 1:                    
+    for r in range(width):
+        for c in range(height):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if r > 0 and grid[r-1][c] == 1:
                     perimeter -= 2
-                if c > 0 and grid[r][c-1] == 1:                    
+                if c > 0 and grid[r][c-1] == 1:
                     perimeter -= 2
     return perimeter
