a9af98a23d49a68676b41082bec844eaea8e56f8
nuel07 <yehanganep@gmail.com>
2022-06-13 06:50:24 -0700

    primegame attempt 1

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""Determine the winner of the prime games"""
+
+
+def isPrime(n: int) -> bool:
+    """determine if a number is prime"""
+    if n <= 1:
+        return False
+    for num in range(2, n):
+        if num % 2 == 0:
+            return False
+    return True
+
+
+def isWinner(x: int, nums: list) -> str:
+    """return winner of the prime games"""
+    M_wins = 0
+    B_wins = 0
+
+    for turn in range(x):
+        for n in range(len(nums)):
+            if isPrime(nums[n]):
+                temp = nums[n]
+                nums.pop(n)
+                for i in nums:
+                    if i % temp == 0:
+                        nums.remove(i)
+                if turn % 2 == 0:
+                    M_wins += 1
+                else:
+                    B_wins += 1
+                break
+            if n + 1 == len(nums) and M_wins == 0 and B_wins == 0:
+                return None
+        if M_wins == B_wins:
+            return None
+        if M_wins > B_wins:
+            return "Maria"
+        return "Ben"
